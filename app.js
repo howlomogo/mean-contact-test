3d2d7e96fb3e37e67c780853d075e01847cc7b50
Matt Pilcher <mattpilcher87@gmail.com>
2017-07-31 23:07:38 +0100

    routing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// ./node_modules/.bin/nodemon <-- nodemon
+
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var bodyparser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
 
@@ -9,8 +11,33 @@ var app = express();
 
 const route = require('./routes/route');
 
+// Connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/contactlist');
+
+// On Connection
+mongoose.connection.on('connected',()=>{
+	console.log('Connect to database mongodb @ 27017');
+});
+
+mongoose.connection.on('error', (err)=>{
+	if(err) {
+		console.log('Error in Databse connection: ' + err);
+	}
+});
+
 // Port Number
 const port = 3000;
+
+// Middleware - CORS
+app.use(cors());
+
+// Middleware - BodyParser
+app.use(bodyparser.json());
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', route);
 
 // Testing Server
 app.get('/', (req, res)=>{
---
+++ routes/route.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const Contact = require('../models/contacts');
+
+
+// Retrieving Contacts
+router.get('/contacts', (req, res, next)=>{
+	Contact.find(function(err, contacts){
+		// contacts retrieved and send to client in Json format
+		res.json(contacts);
+	})
+});
+
+// Adding Contact
+router.post('/contact', (req, res, next)=>{
+	let newContact = new Contact({
+		first_name: req.body.first_name,
+		last_name: req.body.first_name,
+		phone: req.body.phone
+	});
+
+	newContact.save(err, contact)=>{
+		if(err){
+			res.json({msg: 'Failed to add contact'});
+		}
+		else {
+			res.json({msg: 'Contact added succesfully'});
+		}
+	}
+});
+
+// Delete Contact
+router.delete('/contact/:id', (req, res, next)=>{
+	Contact.remove({_id: req.params.id}, function(err, results) {
+		if(err) {
+			res.json(err);
+		}
+		else {
+			res.json(result);
+		}
+	});
+});
+
+module.exports = router;
